92d4a21591d94a42f6a73bd52e211ea6e6aa62be
Imac-tiny <IMAC@example.com>
2021-04-15 18:40:07 +0700

    tt

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main>
         hello world
-         <img src="./image_test.jpg" />
+         <img src="./image_test.jpg" width="150px" />
       </main>
 
       <footer>
